09b7c35cc6a0eba909209d178d64b852af494054
Godfrey Best <godfrey.c.best@gmail.com>
2020-02-15 15:36:21 -0500

    separate out nav component

--- web/src/Header.tsx
+++ web/src/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useMeQuery, useLogoutMutation } from "./generated/graphql";
+import { useMeQuery } from "./generated/graphql";
 import { setAccessToken } from "./accessToken";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -9,7 +9,6 @@ interface Props {}
 
 export const Header: React.FC<Props> = () => {
   const { data, loading } = useMeQuery();
-  const [logout, { client }] = useLogoutMutation();
 
   let body: any = null;
 
@@ -21,35 +20,12 @@ export const Header: React.FC<Props> = () => {
     body = <div>not logged in</div>;
   }
 
-  const handleLogout = async (eventKey: string) => {
-    if(eventKey !== "1") return;
-    await logout();
-    setAccessToken("");
-    await client!.resetStore();
-  };
-
-  const LoginLink = () => (<Nav.Link as={Link} to="/login">Login</Nav.Link>)
-  const LogoutLink = () => (<Nav.Link eventKey="1" as={Link} to="/login">Logout</Nav.Link>)
-  const RegisterLink = () => (<Nav.Link as={Link} to="/register">Register</Nav.Link>)
+  
 
 
   return (
     <header>
-      <Navbar bg="primary" expand="lg">
-        <Navbar.Brand as={Link} to="/">
-          <img src="" alt=""/>
-            Eat It!
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav onSelect={handleLogout}>
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            {!loading && data && data.me ? null : <RegisterLink/>}
-            {!loading && data && data.me ? <LogoutLink/> : <LoginLink/>}
-            <Nav.Link as={Link} to="/bye">Bye</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      
       {body}
     </header>
   );
---
+++ web/src/components/Nav.tsx
@@ -0,0 +1,48 @@
+import React, { Fragment } from 'react';
+import { Link } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { setAccessToken } from "../accessToken";
+import { useLogoutMutation, useMeQuery } from '../generated/graphql';
+
+
+const [logout, { client }] = useLogoutMutation();
+const handleLogout = async (eventKey: string) => {
+  if (eventKey !== "1") return;
+  await logout();
+  setAccessToken("");
+  await client!.resetStore();
+};
+
+const LoginLink = () => (<Nav.Link as={Link} to="/login">Login</Nav.Link>)
+const LogoutLink = () => (<Nav.Link eventKey="1" as={Link} to="/login">Logout</Nav.Link>)
+const RegisterLink = () => (<Nav.Link as={Link} to="/register">Register</Nav.Link>)
+
+
+interface Props { }
+
+export const NavComponent: React.FC<Props> = () => {
+  const { data, loading } = useMeQuery();
+
+  return (
+    <Fragment>
+      <Navbar bg="primary" expand="lg">
+        <Navbar.Brand as={Link} to="/">
+          <img src="" alt="" />
+          Eat It!
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav onSelect={handleLogout}>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            {!loading && data && data.me ? null : <RegisterLink />}
+            {!loading && data && data.me ? <LogoutLink /> : <LoginLink />}
+            <Nav.Link as={Link} to="/bye">Bye</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Fragment>
+  )
+}
+
+export default NavComponent;
