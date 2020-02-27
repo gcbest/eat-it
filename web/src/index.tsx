b36e8a8ff2d09889574aeeb4c6642204f61a191b
Godfrey Best <godfrey.c.best@gmail.com>
2020-02-27 06:00:57 -0500

    start api update, remove neverthrow

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,32 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { ApolloProvider } from "@apollo/react-hooks";
-import { getAccessToken, setAccessToken } from "./accessToken";
-import { App } from "./App";
-import { ApolloClient } from "apollo-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { HttpLink } from "apollo-link-http";
-import { onError } from "apollo-link-error";
-import { ApolloLink, Observable } from "apollo-link";
-import { TokenRefreshLink } from "apollo-link-token-refresh";
-import jwtDecode from "jwt-decode";
-import './assets/css/bootswatch.min.css';
+import React from "react"
+import ReactDOM from "react-dom"
+require('dotenv').config()
+import { ApolloProvider } from "@apollo/react-hooks"
+import { getAccessToken, setAccessToken } from "./accessToken"
+import { App } from "./App"
+import { ApolloClient } from "apollo-client"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { HttpLink } from "apollo-link-http"
+import { onError } from "apollo-link-error"
+import { ApolloLink, Observable } from "apollo-link"
+import { TokenRefreshLink } from "apollo-link-token-refresh"
+import jwtDecode from "jwt-decode"
+import './assets/css/bootswatch.min.css'
 
-const cache = new InMemoryCache({});
+const cache = new InMemoryCache({})
 
 const requestLink = new ApolloLink(
   (operation, forward) =>
     new Observable(observer => {
-      let handle: any;
+      let handle: any
       Promise.resolve(operation)
         .then(operation => {
-          const accessToken = getAccessToken();
+          const accessToken = getAccessToken()
           if (accessToken) {
             operation.setContext({
               headers: {
                 authorization: `bearer ${accessToken}`
               }
-            });
+            })
           }
         })
         .then(() => {
@@ -34,55 +35,55 @@ const requestLink = new ApolloLink(
             next: observer.next.bind(observer),
             error: observer.error.bind(observer),
             complete: observer.complete.bind(observer)
-          });
+          })
         })
-        .catch(observer.error.bind(observer));
+        .catch(observer.error.bind(observer))
 
       return () => {
-        if (handle) handle.unsubscribe();
-      };
+        if (handle) handle.unsubscribe()
+      }
     })
-);
+)
 
 const client = new ApolloClient({
   link: ApolloLink.from([
     new TokenRefreshLink({
       accessTokenField: "accessToken",
       isTokenValidOrUndefined: () => {
-        const token = getAccessToken();
+        const token = getAccessToken()
 
         if (!token) {
-          return true;
+          return true
         }
 
         try {
-          const { exp } = jwtDecode(token);
+          const { exp } = jwtDecode(token)
           if (Date.now() >= exp * 1000) {
-            return false;
+            return false
           } else {
-            return true;
+            return true
           }
         } catch {
-          return false;
+          return false
         }
       },
       fetchAccessToken: () => {
         return fetch("http://localhost:4000/refresh_token", {
           method: "POST",
           credentials: "include"
-        });
+        })
       },
       handleFetch: accessToken => {
-        setAccessToken(accessToken);
+        setAccessToken(accessToken)
       },
       handleError: err => {
-        console.warn("Your refresh token is invalid. Try to relogin");
-        console.error(err);
+        console.warn("Your refresh token is invalid. Try to relogin")
+        console.error(err)
       }
     }),
     onError(({ graphQLErrors, networkError }) => {
-      console.log(graphQLErrors);
-      console.log(networkError);
+      console.log(graphQLErrors)
+      console.log(networkError)
     }),
     requestLink,
     new HttpLink({
@@ -91,11 +92,11 @@ const client = new ApolloClient({
     })
   ]),
   cache
-});
+})
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
   document.getElementById("root")
-);
+)
