10efbba685e5cfd0895dfbcda963bffcb5918f4a
Godfrey Best <godfrey.c.best@gmail.com>
2020-04-15 08:24:47 -0400

    change url base on client

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,12 +4,14 @@ import { setAccessToken } from './lib/accessToken';
 import './styles/App.css'
 
 interface Props { }
+const URL_BASE = 'https://eat--it.herokuapp.com'
 
 export const App: React.FC<Props> = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('http://localhost:4000/refresh_token', {
+    // fetch('http://localhost:4000/refresh_token', {
+    fetch(`${URL_BASE}/refresh_token`, {
       method: 'POST',
       credentials: 'include'
     }).then(async x => {
