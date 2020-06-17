13f11b66d18c8d3e9d04ac78d554253d00d88e3c
Godfrey Best <godfrey.c.best@gmail.com>
2020-06-17 16:33:12 -0400

    remove @bit

--- web/src/lib/cloneDeep.d.ts
+++ web/src/lib/cloneDeep.d.ts
@@ -1,3 +1,3 @@
-declare module '@bit/lodash.lodash.clone-deep' {
-    export default function cloneDeep(object: Object | null): Object | null | any;
-}
+// declare module '@bit/lodash.lodash.clone-deep' {
+//     export default function cloneDeep(object: Object | null): Object | null | any;
+// }
--- web/src/lib/hasIn.d.ts
+++ web/src/lib/hasIn.d.ts
@@ -1,3 +1,3 @@
-declare module '@bit/lodash.lodash.has-in' {
-    export default function hasIn(object: Object | undefined, path: Array | string): boolean;
-}
+// declare module '@bit/lodash.lodash.has-in' {
+//     export default function hasIn(object: Object | undefined, path: Array | string): boolean;
+// }
