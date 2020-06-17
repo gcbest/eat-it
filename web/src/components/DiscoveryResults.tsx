8349ecbaa9ce853f9f998f4a6b432429ef58e8a1
Godfrey Best <godfrey.c.best@gmail.com>
2020-06-17 16:41:36 -0400

    update no results found

--- web/src/components/DiscoveryResults.tsx
+++ web/src/components/DiscoveryResults.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { DiscoveryCard } from './DiscoveryCard'
 import CardDeck from 'react-bootstrap/CardDeck'
 import { Recipe } from 'lib/interfaces';
-import discoverStyles from '../styles/Discover.module.css';
-
 
 interface Props {
     recipes: Recipe[] | Error | undefined
@@ -16,7 +14,7 @@ export const DiscoveryResults: React.FC<Props> = ({ recipes, hasSearched }) => {
 
     return (
         <div>
-            {hasSearched && recipes === null ? <div className={discoverStyles.header}><h5>No Results Found</h5> <h6>Try refreshing the page and performing a new search</h6></div>:
+            {hasSearched && recipes === null ? <h5>No Results Found</h5>:
                 Array.isArray(recipes) && recipes.length > 0 ? <CardDeck id="cardDeck" style={{justifyContent: "space-around"}}> {DiscoveryCardList(recipes)}  </CardDeck> : null}
         </div>
     )
